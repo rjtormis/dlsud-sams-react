3a016087550096657aaef1861c98ef5096530337
R-esentful <raneljohnt@gmail.com>
2023-02-24 23:26:52 +0800

    updated index

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM, { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
