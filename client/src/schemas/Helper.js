cb40c7b8a00469134d87e512076037285c5f1cb9
R-esentful <raneljohnt@gmail.com>
2023-02-26 00:02:01 +0800

    Updated Helper

--- client/src/schemas/Helper.js
+++ client/src/schemas/Helper.js
@@ -1,3 +1,20 @@
-export const name_regex = /^[a-zA-Z]+$/;
+import axios from "axios";
+export const name_regex = /^[A-Za-z]+(?:\s+[A-Za-z]+)*$/;
 export const email_regex = /^\w+@dlsud\.edu\.ph$/;
-export const password_regex = /^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{5,}$/;
+export const password_regex = /^(?=.*[A-Z])(?=.*\d).+$/;
+
+// Helper fetch api for both student_number & emailAddress
+// Since we can't use useFetch in plain JS =(
+export const isAvailable = async (condition, endpoint) => {
+  try {
+    const response = await axios.get(`/api/v1/${endpoint}`, {
+      params: { q: condition },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data.Available;
+  } catch (e) {
+    console.log(e);
+  }
+};
