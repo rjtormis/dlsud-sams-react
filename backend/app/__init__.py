54ed9fa58a0ad3ff0c74e05396ae9f37b0c9f48c
R-esentful <raneljohnt@gmail.com>
2023-02-26 17:41:27 +0800

    updated init file

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -9,8 +9,21 @@ app.config.from_object(ApplicationConfig)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt()
 
+
 # Esential API Imports
 from .api import user
 
 # Essential Model Imports
 from .models.user import User
+from .models.student import Student
+from .models.collegiate import Collegiate
+
+
+# Scripts
+from .scripts.addCollegiate import collegiateScript
+
+# Disable this when developing
+# with app.app_context():
+
+#     db.create_all()
+#     collegiateScript()
