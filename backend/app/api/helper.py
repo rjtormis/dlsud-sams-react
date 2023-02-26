c398f785caaab24bc7751896df69ec967148f78e
R-esentful <raneljohnt@gmail.com>
2023-02-26 17:41:41 +0800

    Updated api's

--- backend/app/api/helper.py
+++
@@ -1,3 +0,0 @@
-def account_type(type):
-    """Account Type"""
-    return "student" if type == "student" else "professor"
--- backend/app/api/user.py
+++ backend/app/api/user.py
@@ -3,9 +3,12 @@ from flask import request, jsonify
 
 # Models
 from ..models.user import User
+from ..models.student import Student
+from ..models.professor import Professor
+from ..models.collegiate import Collegiate
 
 # Helper
-from .helper import account_type
+from ..utils.push_to_database import push_to_database
 
 
 @app.route("/api/v1/users", methods=["GET", "POST"])
@@ -15,6 +18,7 @@ def users():
         return jsonify({"OK": "KEYO!"})
 
     if request.method == "POST":
+
         data = request.get_json()
         (
             first_name,
@@ -34,18 +38,52 @@ def users():
             data["confirmPassword"],
         )
 
-        new_user = User(
-            first_name=first_name,
-            middle_initial=middle_initial,
-            last_name=last_name,
-            type=account_type(type),
-            emailAddress=emailAddress,
-            password=confirmPassword,
-        )
+        if type == "student":
 
-        print(new_user)
+            id = data["studentNumber"]
 
-        # db.session.add(new_user)
-        # db.session.commit()
+            qEmail = User.query.filter_by(emailAddress=emailAddress).first()
+            query_id = Student.query.filter_by(student_no=id).first()
+
+            if qEmail and query_id:
+                return jsonify({"msg": "Student number & Email already taken."}), 409
+
+            if query_id:
+                return jsonify({"msg": "Student no. already taken."}), 409
+
+            if qEmail:
+                return jsonify({"msg": "Email already taken."}), 409
+
+            new_student = Student(
+                first_name=first_name,
+                middle_initial=middle_initial,
+                last_name=last_name,
+                student_no=id,
+                type=type,
+                emailAddress=emailAddress,
+                password=confirmPassword,
+            )
+
+            push_to_database(new_student)
+
+        if type == "professor":
+
+            collegiate_shorten = data["collegiate"]
+
+            collegiate = Collegiate.query.filter_by(
+                collegiate_shorten=collegiate_shorten
+            ).first()
+
+            new_professor = Professor(
+                first_name=first_name,
+                middle_initial=middle_initial,
+                last_name=last_name,
+                type=type,
+                collegiate_id=collegiate.id,
+                emailAddress=emailAddress,
+                password=confirmPassword,
+            )
+
+            push_to_database(new_professor)
 
         return jsonify({"msg": "Successful"}), 201
