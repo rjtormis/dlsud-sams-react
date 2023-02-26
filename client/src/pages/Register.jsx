5bb62e135f0a94459b72f2d71d72dea5f50f9122
R-esentful <raneljohnt@gmail.com>
2023-02-26 21:30:27 +0800

    Added responsive design to register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,6 +2,8 @@ import { useParams, Link } from "react-router-dom";
 import { FaHome, FaUserTie, FaUserGraduate } from "react-icons/fa";
 import { useState } from "react";
 
+import logo from "../assets/dlsu-d.png";
+
 import RegisterStudent from "../components/Register/RegisterStudent";
 import RegisterProfessor from "../components/Register/RegisterProfessor";
 import Alert from "../components/Shared/Alert";
@@ -23,41 +25,60 @@ function Register() {
   const type = params.type;
 
   return (
-    <div className="grid grid-cols-4 h-screen">
-      <div id="regBG" className="p-4 col-span-1 flex content-center justify-center "></div>
-      <div className="col-span-3 bg-white p-20 flex flex-col justify-center relative">
-        <div className="flex flex-col absolute top-3 right-3">
-          <div className="tooltip tooltip-neutral tooltip-left" data-tip="Home">
-            <Link to="/" className="btn btn-ghost">
-              <FaHome color="#224429" size={18} />
+    <div className="grid grid-cols-1 h-screen lg:grid-cols-3 xl:grid-cols-4">
+      <div id="regBG" className="hidden lg:block col-span-1 xl:col-span-1 "></div>
+      <div className="col-span-1 bg-white flex flex-col justify-center relative lg:col-span-2 p-8 xl:col-span-3">
+        <div className="xl:absolute top-5 left-15 ">
+          <img src={logo} className="w-48 m-auto sm:w-60" alt="" />
+        </div>
+
+        <div className="text-center xl:absolute top-3 right-3">
+          <div className="mr-2 tooltip tooltip-neutral tooltip-left" data-tip="Home">
+            <Link to="/" className="link link-primary xl:hidden">
+              Home
+            </Link>
+            <Link to="/" className="hidden xl:btn btn-ghost">
+              <FaHome color="#224429" size={25} />
             </Link>
           </div>
           <div
-            className="mt-2 tooltip tooltip-neutral tooltip-left"
+            className="ml-2 tooltip tooltip-neutral tooltip-left"
             data-tip={`Register as ${type === "student" ? "professor" : "student"} `}
           >
             <Link
               to={`/register/${type === "student" ? "professor" : "student"}`}
-              className="btn btn-ghost "
+              className="link link-primary xl:hidden"
+            >
+              {type === "student"
+                ? // <FaUserGraduate color="#224429" size={25} />
+                  `As ${type === "student" ? "professor" : "student"}`
+                : // <FaUserTie color="#224429" size={25} />
+                  `As ${type === "student" ? "professor" : "student"}`}
+            </Link>
+
+            <Link
+              to={`/register/${type === "student" ? "professor" : "student"}`}
+              className="hidden xl:btn btn-ghost"
             >
               {type === "student" ? (
-                <FaUserGraduate color="#224429" />
+                <FaUserGraduate color="#224429" size={25} />
               ) : (
-                <FaUserTie color="#224429" />
+                <FaUserTie color="#224429" size={25} />
               )}
             </Link>
           </div>
         </div>
-
         {/* RESTRUCTURE LATER */}
-        {success ? <Alert /> : null}
+        {success ? <Alert custom={"mt-4"} /> : null}
 
-        <section id="regInput" className="p-0">
-          <h2 className="text-green-700 text-4xl mt-4">
+        <section id="regInput" className="p-0 relative top-0 left-0 xl:px-20">
+          <h1 className="text-green-700  mt-4 text-3xl text-center sm:text-center md:text-center xl:text-start">
             CREATE A {type === "student" ? "STUDENT" : "PROFESSOR"} ACCOUNT
-          </h2>
+          </h1>
 
-          <p className="mt-2">Never Miss a Beat: Keep Track of Attendance with Ease</p>
+          <p className="mt-2 text-center xl:text-start">
+            Never Miss a Beat: Keep Track of Attendance with Ease
+          </p>
 
           {type === "student" ? (
             <RegisterStudent handleSuccess={handleSuccess} />
