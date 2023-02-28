b43fe316ed74bcbc48d0fb1718ed842137a26ed1
R-esentful <raneljohnt@gmail.com>
2023-03-01 00:23:42 +0800

    Updated index

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,14 @@ import ReactDOM, { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Context
+import { AuthContextProvider } from "./context/AuthContext";
+
 const root = createRoot(document.getElementById("root"));
 root.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
   // <React.StrictMode>
-  <App />
   // </React.StrictMode>
 );
