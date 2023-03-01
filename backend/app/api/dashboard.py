2017c0e1e1db564e3e146e9beb5b0e50ed005d15
R-esentful <raneljohnt@gmail.com>
2023-03-01 23:51:15 +0800

    Updated api

--- backend/app/api/dashboard.py
+++ backend/app/api/dashboard.py
@@ -3,7 +3,7 @@ from app import app, db, jwt_required, get_jwt_identity
 from ..models.student import Student
 
 
-@app.route("/api/dashboard", methods=["GET"])
+@app.route("/api/v1/dashboard", methods=["GET"])
 @jwt_required()
 def dashboard():
     current_user = get_jwt_identity()
@@ -12,3 +12,11 @@ def dashboard():
     return jsonify(
         {"total": {"students": total_student, "lectures": 1, "classrooms": 999}}
     )
+
+
+@app.route("/api/v1/classroom", methods=["POST"])
+@jwt_required()
+def classroom():
+    data = request.get_json()
+    print(data)
+    return jsonify({"msg": "recieved"})
