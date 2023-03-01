8b10ab2fc88a7c6f063dbf0ac241f1a38765d839
R-esentful <raneljohnt@gmail.com>
2023-03-01 23:51:36 +0800

    Updated component

--- client/src/components/Shared/Modal.jsx
+++ client/src/components/Shared/Modal.jsx
@@ -1,11 +1,10 @@
 import { Link } from "react-router-dom";
 function Modal({ id, children }) {
-  console.log(id);
   return (
     <>
       <div className="modal" id={id}>
         <div className="modal-box w-4/12 relative">
-          <a href="#" className="btn btn-sm btn-circle absolute right-2 top-2">
+          <a href="#" className="btn btn-sm btn-circle absolute right-2 top-2 hover:btn-error">
             ✕
           </a>
           {children}
