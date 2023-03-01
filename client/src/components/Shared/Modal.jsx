5da75845d4894497f8b7d4f5d1203013d77fe5d8
R-esentful <raneljohnt@gmail.com>
2023-03-01 11:24:41 +0800

    Updated Modal

--- client/src/components/Shared/Modal.jsx
+++ client/src/components/Shared/Modal.jsx
@@ -1,21 +1,14 @@
-import React from "react";
-
-function Modal() {
+import { Link } from "react-router-dom";
+function Modal({ id, children }) {
+  console.log(id);
   return (
     <>
-      <input type="checkbox" id="my-modal" class="modal-toggle" />
-      <div className="modal">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to use Wikipedia for
-            free!
-          </p>
-          <div className="modal-action">
-            <label htmlFor="my-modal" className="btn">
-              Yay!
-            </label>
-          </div>
+      <div className="modal" id={id}>
+        <div className="modal-box w-4/12 relative">
+          <a href="#" className="btn btn-sm btn-circle absolute right-2 top-2">
+            ✕
+          </a>
+          {children}
         </div>
       </div>
     </>
