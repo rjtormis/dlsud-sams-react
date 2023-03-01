fe1fae7f1f0d997de40f627025e66f3826321202
R-esentful <raneljohnt@gmail.com>
2023-03-01 11:23:53 +0800

    Updated dashboard api

--- backend/app/api/dashboard.py
+++ backend/app/api/dashboard.py
@@ -1,11 +1,12 @@
 from flask import jsonify, request
-from app import app, db, jwt_required
+from app import app, db, jwt_required, get_jwt_identity
 from ..models.student import Student
 
 
 @app.route("/api/dashboard", methods=["GET"])
+@jwt_required()
 def dashboard():
-
+    current_user = get_jwt_identity()
     total_student = Student.query.count()
 
     return jsonify(
