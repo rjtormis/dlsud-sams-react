edeeb99ab20d8012cffe9f36ac82555527274eb5
R-esentful <raneljohnt@gmail.com>
2023-03-03 22:51:30 +0800

    Updated init

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
 from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_refresh_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
