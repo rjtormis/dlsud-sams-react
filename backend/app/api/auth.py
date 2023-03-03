18c6f34b00958a33a2207aabfcf4123072d1491a
R-esentful <raneljohnt@gmail.com>
2023-03-03 22:51:41 +0800

    Updated API

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -1,5 +1,12 @@
-from flask import jsonify, request
+from flask import jsonify, request, make_response
+
 from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_refresh_token
+from flask_jwt_extended import set_access_cookies
+from flask_jwt_extended import set_refresh_cookies
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
 from app import app, db, jwt, bcrypt
 
 # Models
@@ -21,17 +28,38 @@ def auth():
             query_email.password_hash, password
         )
         if confirm_password:
-            access_token = create_access_token(identity=query_email.id)
-            return jsonify(
-                {
-                    "id": query_email.id,
-                    "type": query_email.type,
-                    "access_token": access_token,
-                }
-            )
+            access_token = create_access_token(identity=query_email.id, fresh=True)
+            refresh_token = create_refresh_token(identity=query_email.id)
+
+            response = make_response("loggedIn")
+            response.set_cookie("id", str(query_email.id), httponly=True)
+            set_access_cookies(response, access_token)
+            set_refresh_cookies(response, refresh_token)
+
+            return response, 200
 
         else:
             return jsonify({"msg": "Invalid credentials"}), 404
 
     else:
         return jsonify({"msg": "Invalid credentials"}), 404
+
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    response = make_response("logout")
+    response.set_cookie("access_token_cookie", "", max_age=0)
+    response.set_cookie("refresh_token_cookie", "", max_age=0)
+    response.set_cookie("csrf_refresh_token", "", max_age=0)
+    response.set_cookie("csrf_access_token", "", max_age=0)
+    response.set_cookie("id", "", max_age=0)
+    return response, 200
+
+
+@app.route("/refresh_token", methods=["POST"])
+@jwt_required(refresh=True)
+def refresh():
+    current_user = get_jwt_identity()
+    query_id = User.query.filter_by(id=current_user).first()
+    access_token = create_access_token(identity=query_id.id)
+    return jsonify({"access_token": access_token})
--- backend/app/api/dashboard.py
+++ backend/app/api/dashboard.py
@@ -8,7 +8,8 @@ from ..models.student import Student
 def dashboard():
     current_user = get_jwt_identity()
     total_student = Student.query.count()
-
+    if not current_user:
+        return jsonify({"msg": "UNAUTHORIZED"}), 401
     return jsonify(
         {"total": {"students": total_student, "lectures": 1, "classrooms": 999}}
     )
