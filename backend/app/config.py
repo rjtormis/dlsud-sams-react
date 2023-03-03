82ef987a1a37f9202d54712c732f8b79916b044a
R-esentful <raneljohnt@gmail.com>
2023-03-03 22:51:51 +0800

    Updated config

--- backend/app/config.py
+++ backend/app/config.py
@@ -9,3 +9,12 @@ class ApplicationConfig:
     SECRET_KEY = os.getenv("SECRET_KEY")
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.getenv("db")
+
+    JWT_TOKEN_LOCATION = ["cookies"]
+    JWT_SECRET_KEY = os.getenv("SECRET_KEY")
+    JWT_ACCESS_COOKIE_NAME = "access_token_cookie"
+    JWT_ACCESS_COOKIE_PATH = "/"
+    JWT_COOKIE_CSRF_PROTECT = True
+    JWT_COOKIE_SECURE = True
+    JWT_REFRESH_COOKIE_NAME = "refresh_token_cookie"
+    JWT_REFRESH_COOKIE_PATH = "/"
