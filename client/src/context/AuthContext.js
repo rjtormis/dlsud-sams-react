ba8533bfb3a887c84bd04ed9dd06968990e16613
R-esentful <raneljohnt@gmail.com>
2023-03-04 15:52:08 +0800

    Updated context

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,14 +1,48 @@
-import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+import { createContext, useEffect, useRef, useState } from "react";
+import useCookie from "../hooks/useCookie";
 
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  const [auth, setAuth] = useState({});
+  const [user, setUser] = useState("");
+  const refresh = useCookie("csrf_refresh_token");
+  const [auth, setAuth] = useState(() => {
+    return JSON.parse(localStorage.getItem("user"));
+  });
+
   useEffect(() => {
     localStorage.setItem("user", JSON.stringify(auth));
+    if (auth) {
+      setUser(auth.name);
+    }
   }, [auth]);
 
-  return <AuthContext.Provider value={{ auth, setAuth }}>{children}</AuthContext.Provider>;
+  useEffect(() => {
+    if (auth && refresh) {
+      const requestAccessToken = setInterval(async () => {
+        try {
+          const response = await axios.post("/refresh_token", null, {
+            headers: { "X-CSRF-TOKEN": refresh },
+          });
+          console.log(response);
+        } catch (e) {
+          console.log(e);
+        }
+      }, 1000 * 60 * 8);
+      return () => clearInterval(requestAccessToken);
+    }
+  }, [auth, refresh]);
+
+  const logout = () => {
+    setAuth(null);
+    setUser(null);
+    localStorage.removeItem("user");
+  };
+
+  return (
+    <AuthContext.Provider value={{ auth, user, setAuth, logout }}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthContext;
