0ce9c1ccf4f3f2290a43dbf24cd3008f44789a7d
R-esentful <raneljohnt@gmail.com>
2023-03-04 15:51:18 +0800

    Updated config

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from datetime import timedelta
 
 load_dotenv()
 
@@ -9,8 +10,12 @@ class ApplicationConfig:
     SECRET_KEY = os.getenv("SECRET_KEY")
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.getenv("db")
+    SQLALCHEMY_POOL_SIZE = 10
+    SQLALCHEMY_POOL_TIMEOUT = 30
 
     JWT_TOKEN_LOCATION = ["cookies"]
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=10)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(hours=10)
     JWT_SECRET_KEY = os.getenv("SECRET_KEY")
     JWT_ACCESS_COOKIE_NAME = "access_token_cookie"
     JWT_ACCESS_COOKIE_PATH = "/"
