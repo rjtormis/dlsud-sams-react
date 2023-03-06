e560564eceed5a115513aea288edbdd75ed7a953
R-esentful <raneljohnt@gmail.com>
2023-03-06 20:43:53 +0800

    Updated hooks

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import AuthContext from "../context/AuthContext";
+import AuthContext from "../context/Authentication/AuthContext";
 
 function useAuth() {
   return useContext(AuthContext);
