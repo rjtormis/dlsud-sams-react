eea837efcec5523bd1493920cc3c7967f3883ed3
R-esentful <raneljohnt@gmail.com>
2023-03-06 20:43:08 +0800

    Updated models

--- backend/app/models/section.py
+++ backend/app/models/section.py
@@ -7,7 +7,10 @@ class Section(db.Model, Details):
 
     id = db.Column(db.Integer(), primary_key=True)
     section_adviser = db.Column(db.String(length=40), db.ForeignKey("professors.id"))
-    section_name = db.Column(db.String(length=30), unique=True, nullable=False)
+    section_full = db.Column(db.String(length=30), unique=True, nullable=False)
+    section_course = db.Column(db.String(length=10), nullable=False)
+    section_year = db.Column(db.String(length=1), nullable=False)
+    section_level = db.Column(db.String(length=1), nullable=False)
     section_image = db.Column(db.String(length=100))
 
     subjects = db.relationship("Subject", backref="section")
@@ -15,12 +18,15 @@ class Section(db.Model, Details):
     def json_format(self):
         return {
             "id": self.id,
-            "section_name": self.section_name,
+            "section_full": self.section_full,
             "section_adviser": f"{self.professor.first_name} {self.professor.middle_initial} {self.professor.last_name}",
+            "section_course": self.section_course,
+            "section_year": self.section_year,
+            "section_level": self.section_full,
             "section_image": self.section_image,
             "created": self.created,
             "updated": self.updated,
         }
 
     def __repr__(self) -> str:
-        return f"Section {self.section_name} Adviser: {self.professor.first_name} {self.professor.middle_initial} {self.professor.last_name}"
+        return f"Section {self.section_full} Adviser: {self.professor.first_name} {self.professor.middle_initial} {self.professor.last_name}"
