253770d3d46b098375a194d328b500f29ae488dc
R-esentful <raneljohnt@gmail.com>
2023-03-09 21:06:18 +0800

    Updated layouts

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -9,6 +9,7 @@ import profile from "../assets/sample-profile.jfif";
 
 // Hooks
 import useAuth from "../hooks/useAuth";
+import { SpecificSectionContextProvider } from "../context/Dashboard/SpecificSection/SpecificSectionContext";
 
 function DashboardLayout() {
   const navigate = useNavigate();
@@ -24,75 +25,77 @@ function DashboardLayout() {
     } catch (e) {}
   };
   return (
-    <div id="dashboard-container" className="h-screen flex flex-col">
-      <header className="h-12 p-2 relative shadow-sm" style={{ backgroundColor: "#FAFAFA" }}>
-        <div className="flex">
-          <div className="absolute top-2 left-2">
-            <img src={logo} alt="DLSUD LOGO" className="w-36" />
-          </div>
-
-          <div className="ml-auto avatar dropdown dropdown-end hover:cursor-pointer">
-            <div className="w-8 h-8 rounded-xl" tabIndex={0}>
-              <img src={profile} alt="" className="" />
+    <SpecificSectionContextProvider>
+      <div id="dashboard-container" className="h-screen flex flex-col">
+        <header className="h-12 p-2 relative shadow-sm" style={{ backgroundColor: "#FAFAFA" }}>
+          <div className="flex">
+            <div className="absolute top-2 left-2">
+              <img src={logo} alt="DLSUD LOGO" className="w-36" />
             </div>
-            <ul
-              tabIndex={0}
-              className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a>Item 1</a>
+
+            <div className="ml-auto avatar dropdown dropdown-end hover:cursor-pointer">
+              <div className="w-8 h-8 rounded-xl" tabIndex={0}>
+                <img src={profile} alt="" className="" />
+              </div>
+              <ul
+                tabIndex={0}
+                className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a>Item 1</a>
+                </li>
+                <li>
+                  <a>Item 2</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </header>
+
+        <div className="flex-1 flex">
+          <div className="p-1 relative shadow" style={{ backgroundColor: "#FAFAFA" }}>
+            <ul className="flex flex-col">
+              <li className="tooltip tooltip-right tooltip-primary" data-tip="Dashboard">
+                <Link to="/dashboard" className="flex btn btn-ghost btn-square">
+                  <div>
+                    <MdDashboard color="#224429" size={25} className="block m-auto" />
+                  </div>
+                </Link>
               </li>
-              <li>
-                <a>Item 2</a>
+              <li className="mt-4 tooltip tooltip-right tooltip-primary" data-tip="Classroom">
+                <Link to="/dashboard/sections" className="flex btn btn-ghost btn-square">
+                  <div>
+                    <HiUserGroup color="#224429" size={25} className="block m-auto" />
+                  </div>
+                </Link>
+              </li>
+              <li className="mt-4 tooltip tooltip-right tooltip-primary" data-tip="Profile">
+                <Link to="dashboard/profile" className="flex btn btn-ghost btn-square">
+                  <div>
+                    <RiUserSettingsFill color="#224429" size={25} className="block m-auto" />
+                  </div>
+                </Link>
+              </li>
+              <li
+                className="mt-4 tooltip tooltip-right absolute bottom-4 tooltip-primary"
+                data-tip="Logout"
+              >
+                <button onClick={handleLogout} className="flex btn btn-ghost btn-square">
+                  <div>
+                    <RiLogoutBoxRFill color="#224429" size={25} className="block m-auto" />
+                  </div>
+                </button>
               </li>
             </ul>
           </div>
-        </div>
-      </header>
 
-      <div className="flex-1 flex">
-        <div className="p-1 relative shadow" style={{ backgroundColor: "#FAFAFA" }}>
-          <ul className="flex flex-col">
-            <li className="tooltip tooltip-right tooltip-primary" data-tip="Dashboard">
-              <Link to="/dashboard" className="flex btn btn-ghost btn-square">
-                <div>
-                  <MdDashboard color="#224429" size={25} className="block m-auto" />
-                </div>
-              </Link>
-            </li>
-            <li className="mt-4 tooltip tooltip-right tooltip-primary" data-tip="Classroom">
-              <Link to="/dashboard/sections" className="flex btn btn-ghost btn-square">
-                <div>
-                  <HiUserGroup color="#224429" size={25} className="block m-auto" />
-                </div>
-              </Link>
-            </li>
-            <li className="mt-4 tooltip tooltip-right tooltip-primary" data-tip="Profile">
-              <Link to="dashboard/profile" className="flex btn btn-ghost btn-square">
-                <div>
-                  <RiUserSettingsFill color="#224429" size={25} className="block m-auto" />
-                </div>
-              </Link>
-            </li>
-            <li
-              className="mt-4 tooltip tooltip-right absolute bottom-4 tooltip-primary"
-              data-tip="Logout"
-            >
-              <button onClick={handleLogout} className="flex btn btn-ghost btn-square">
-                <div>
-                  <RiLogoutBoxRFill color="#224429" size={25} className="block m-auto" />
-                </div>
-              </button>
-            </li>
-          </ul>
-        </div>
-
-        {/* INSERT HERE */}
-        <div id="dashboard-main" className="flex-1 flex flex-col  p-5">
-          <Outlet />
+          {/* INSERT HERE */}
+          <div id="dashboard-main" className="flex-1 flex flex-col  p-5">
+            <Outlet />
+          </div>
         </div>
       </div>
-    </div>
+    </SpecificSectionContextProvider>
   );
 }
 export default DashboardLayout;
