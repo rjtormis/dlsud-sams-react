292efcfecd5c7c33d9a37979aa7c3ad652a3cb71
R-esentful <raneljohnt@gmail.com>
2023-03-11 19:08:31 +0800

    Updated shared components

--- client/src/components/Shared/CustomInput.jsx
+++ client/src/components/Shared/CustomInput.jsx
@@ -6,7 +6,6 @@ function CustomInput({ label, page, ...props }) {
   const [fields, meta] = useField(props);
   const isError = meta.error && meta.touched;
   const errorMessage = meta.error;
-
   return (
     <FormControl label={label} page={page} error={isError} msg={errorMessage}>
       <Input styles={isError ? "input-error" : "input-ghost"} {...fields} {...props} />
