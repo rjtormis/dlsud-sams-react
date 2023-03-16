f893ef4694d3d5e0b0f0c8afc64ae9644a0e431e
R-esentful <raneljohnt@gmail.com>
2023-03-16 22:34:19 +0800

    Updated models

--- backend/app/models/profile.py
+++ backend/app/models/profile.py
@@ -1,5 +1,6 @@
 from app import db
 from flask import jsonify
+from datetime import datetime
 
 # Models
 from ..models.details import Details
@@ -35,6 +36,40 @@ class ProfessorProfile(db.Model, Details):
                 },
             }
         }
+
+    def update_professor_profile(
+        self,
+        name,
+        bio,
+        collegiate_id,
+        consultation,
+        fb,
+        instagram,
+        linkedIn,
+        twitter,
+    ):
+        """
+        Updates the user model.
+        """
+
+        name_split_reverse = name.split(" ")[::-1]
+        f_name = name_split_reverse[2:][::-1]
+
+        self.professor_profile.first_name = " ".join(f_name)
+        self.professor_profile.middle_initial = name_split_reverse[1]
+        self.professor_profile.last_name = name_split_reverse[0]
+        # self.professor_profile.profile_image_link = image_link
+
+        self.bio = bio
+        self.collegiate = collegiate_id
+        self.consultation = consultation
+        self.fb_social = fb
+        self.instagram_social = instagram
+        self.linkedIn_social = linkedIn
+        self.twitter_social = twitter
+        self.updated = datetime.utcnow()
+
+        db.session.commit()
 
     def __repr__(self):
         return f"Profile: {self.id}"
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,4 +1,5 @@
-from app import db, bcrypt
+import boto3
+from app import db, bcrypt, s3, s3_resource
 from datetime import datetime
 
 # Helper
@@ -16,6 +17,10 @@ class User(db.Model, Details):
     middle_initial = db.Column(db.String(length=1), nullable=False)
     last_name = db.Column(db.String(length=20), nullable=False)
     emailAddress = db.Column(db.String(length=345), nullable=False, unique=True)
+    profile_image_link = db.Column(
+        db.String(length=345),
+        default="https://aws-sams-storage.s3.ap-southeast-1.amazonaws.com/user/default_profile.jpg",
+    )
     password_hash = db.Column(db.String(length=300), nullable=False)
     type = db.Column(db.String(length=15), nullable=False)
 
@@ -30,6 +35,25 @@ class User(db.Model, Details):
         self.password_hash = bcrypt.generate_password_hash(input_password).decode(
             "utf-8"
         )
+
+    def check_user_folder(self, bucket_name, folder_path):
+
+        """
+        Check if the folder of the user already exists in AWS s3
+
+        """
+
+        bucket = s3_resource.Bucket(bucket_name)
+
+        objs = list(bucket.objects.filter(Prefix=folder_path))
+        if len(objs) > 0 and objs[0].key == folder_path + "/":
+            return True
+
+        try:
+            s3.put_object(Bucket=bucket_name, Key=(folder_path + "/"))
+        except Exception as e:
+            print("Error occured")
+            return False
 
     def __repr__(self):
         return f"User: {self.first_name} {self.middle_initial} {self.last_name}, Type:{self.type}"
