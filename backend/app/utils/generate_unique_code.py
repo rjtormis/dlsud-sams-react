b857e7b306ac8202b9ac972f1cbcc57023350f2d
R-esentful <raneljohnt@gmail.com>
2023-03-17 20:54:49 +0800

    Updated utilities

--- backend/app/utils/generate_unique_code.py
+++ backend/app/utils/generate_unique_code.py
@@ -6,3 +6,7 @@ def id_generator(char=string.ascii_uppercase + string.digits):
     code1 = "".join(random.choice(char) for _ in range(4))
     code2 = "".join(random.choice(char) for _ in range(4))
     return f"{code1}-{code2}"
+
+
+def unique_identifier_file(char=string.ascii_uppercase + string.digits):
+    return "".join(random.choice(char) for _ in range(2))
