168eb02f41f427a8008ba7321a7fb566a3a6046c
R-esentful <raneljohnt@gmail.com>
2023-03-24 23:24:05 +0800

    Added exception handlers

---
+++ backend/app/exception.py
@@ -0,0 +1,22 @@
+class UnAuthorizedError(Exception):
+    status_code = 401
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class NotFound(Exception):
+    status_code = 404
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ConflictError(Exception):
+    status_code = 409
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
