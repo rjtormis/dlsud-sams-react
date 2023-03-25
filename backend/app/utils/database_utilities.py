efee95aa5e9973c3a2ee6a91fa3d686786de4dc1
R-esentful <raneljohnt@gmail.com>
2023-03-25 17:08:44 +0800

    Updated utils

--- backend/app/utils/database_utilities.py
+++ backend/app/utils/database_utilities.py
@@ -12,10 +12,21 @@ def push_to_database(data):
         return jsonify({"msg": "Error occured"}), 500
 
 
-def remove_in_database(data):
+def push_multiple_to_database(data1, data2):
+    """Helper Function to push data directly into the database"""
+    try:
+
+        db.session.add(data1)
+        db.session.add(data2)
+        db.session.commit()
+    except:
+        return jsonify({"msg": "Error occured"}), 500
+
+
+def delete_in_database(data):
     """Helper Function that removes data directly into the database."""
     try:
-        db.session.remove(data)
+        db.session.delete(data)
         db.session.commit()
     except:
         return jsonify({"msg": "Error occured"}), 500
