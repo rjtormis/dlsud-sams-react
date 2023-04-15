81c8b996d0fb086b9cc3fef9be5232c61bb811e1
R-esentful <raneljohnt@gmail.com>
2023-04-15 19:29:27 +0800

    Updated layouts

--- client/src/layouts/StudentDashboardLayout.jsx
+++ client/src/layouts/StudentDashboardLayout.jsx
@@ -1,69 +1,62 @@
-import { Helmet } from "react-helmet";
-// import "../styles/student.css";
+import { Outlet } from "react-router-dom";
+import { MdDashboard } from "react-icons/md";
+import { HiUserGroup } from "react-icons/hi";
+import { RiUserSettingsFill, RiLogoutBoxRFill } from "react-icons/ri";
 import logo from "../assets/resized-logo.png";
 import profile from "../assets/spiderman.png";
 function StudentDashboardLayout() {
   return (
     <>
-      <Helmet>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/fork-awesome@1.2.0/css/fork-awesome.min.css"
-          integrity="sha256-XoaMnoYC5TH6/+ihMEnospgm0J1PM/nioxbOUdnM8HY="
-          crossorigin="anonymous"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-        />
-      </Helmet>
-      <header class="dashboard-header">
-        <div class="dashboard-navigation">
-          <button id="menu-btn" class="menu-btn">
-            <span class="material-symbols-rounded menu">menu</span>
-          </button>
-        </div>
-        <div class="dashboard-profile">
-          <a href="profile.html">
-            <img class="student-image" src={profile} alt="student profile" />
-          </a>
-        </div>
-      </header>
-
-      <aside class="sidebar" id="sidebar">
-        <div class="sidebar-logo">
-          <img src={logo} alt="" />
-          <div class="close-btn" id="close-btn">
-            <span class="close material-symbols-rounded add">close</span>
+      <div className="h-screen flex flex-col">
+        <header className="bg-sd-header h-[60px] flex justify-end border-b-[1px] border-solid border-[#35353530] ">
+          <div className="ml-auto my-auto mr-4">
+            <div className="avatar ">
+              <div className="w-[40px] h-[40px] rounded-full border-secondary border-[1px] border-solid bg-white">
+                <img src={profile} alt="student profile" className="max-w-full max-h-full" />
+              </div>
+            </div>
           </div>
-        </div>
-        <div class="sidebar-list">
-          <a href="student_dashboard.html" class="sidebar-link">
-            <span class="material-symbols-rounded">dashboard</span>
-            <h3>Dashboard</h3>
-          </a>
-          <a href="classroom.html" class="sidebar-link">
-            <span class="material-symbols-rounded">book</span>
-            <h3>Classroom</h3>
-          </a>
-          <a href="profile.html" class="sidebar-link">
-            <span class="material-symbols-rounded">account_circle</span>
-            <h3>Profile</h3>
-          </a>
-          <a href="upload.html" class="sidebar-link">
-            <span class="material-symbols-rounded">add_photo_alternate</span>
-            <h3>Upload Image</h3>
-          </a>
-          <a href="settings.html" class="sidebar-link">
-            <span class="material-symbols-rounded">settings</span>
-            <h3>Settings</h3>
-          </a>
-          <a href="#" class="sidebar-link logout">
-            <span class="material-symbols-rounded ">logout</span>
-            <h3>Logout</h3>
-          </a>
-        </div>
-      </aside>
+        </header>
+        <aside className="bg-white fixed p-2 top-0 left-0 bottom-0 border-r-[1px] border-solid border-[#b4b3b3a8] shadow-[3px_3px_3px_rgba(0, 0, 0, 0.049)]">
+          <div className=" mb-4 flex justify-center">
+            <img src={logo} alt="" className="w-[50px]" />
+          </div>
+          <div className="flex flex-col">
+            <div
+              className="tooltip tooltip-right tooltip-secondary mx-auto mb-3"
+              data-tip="Dashboard"
+            >
+              <button className="btn btn-ghost">
+                <MdDashboard color="bg-secondary" size={25} />
+              </button>
+            </div>
+            <div
+              className="tooltip tooltip-right tooltip-secondary mx-auto mb-3"
+              data-tip="Classroom"
+            >
+              <button className="btn btn-ghost">
+                <HiUserGroup color="bg-secondary" size={25} />
+              </button>
+            </div>
+            <div className="tooltip tooltip-right tooltip-secondary mx-auto" data-tip="Profile">
+              <button className="btn btn-ghost">
+                <RiUserSettingsFill color="bg-secondary" size={25} />
+              </button>
+            </div>
+
+            <div
+              className="tooltip tooltip-right tooltip-secondary mx-auto absolute bottom-4"
+              data-tip="Logout"
+            >
+              <button className="btn btn-ghost">
+                <RiLogoutBoxRFill color="bg-secondary" size={25} />
+              </button>
+            </div>
+          </div>
+        </aside>
+
+        <Outlet />
+      </div>
     </>
   );
 }
