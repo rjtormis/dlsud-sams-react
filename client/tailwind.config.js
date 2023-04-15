8c2836101aeab357a55d1ac8d4d73cf187cf0396
R-esentful <raneljohnt@gmail.com>
2023-04-15 19:29:43 +0800

    Updated tailwin config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,33 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	theme: {
-		extend: {},
-	},
-	daisyui: {
-		themes: [
-			{
-				mytheme: {
-					primary: '#224429',
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "sd-header": "url('assets/patterns/1.svg')",
+      },
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#224429",
 
-					secondary: '#436147',
+          secondary: "#436147",
 
-					accent: '#57c12a',
+          accent: "#57c12a",
 
-					neutral: '#fafafa',
+          neutral: "#fafafa",
 
-					'base-100': '#F9F9FB',
+          "base-100": "#F9F9FB",
 
-					info: '#6592CD',
+          info: "#6592CD",
 
-					success: '#72E9A7',
+          success: "#72E9A7",
 
-					warning: '#E68405',
+          warning: "#E68405",
 
-					error: '#E94951',
-				},
-			},
-		],
-	},
-	plugins: [require('daisyui')],
+          error: "#E94951",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
