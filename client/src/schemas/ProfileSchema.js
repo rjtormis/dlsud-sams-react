9ecc396a0b22fc000e5542728eb791324c0655d2
R-esentful <raneljohnt@gmail.com>
2023-04-25 11:58:52 +0800

    Updated schemas

--- client/src/schemas/ProfileSchema.js
+++ client/src/schemas/ProfileSchema.js
@@ -8,6 +8,16 @@ export const profileSchema = yup.object().shape({
   socials: yup.object().shape({
     fb: yup.string().max(50, "Maximum of 50 characters only."),
     instagram: yup.string().max(50, "Maximum of 50 characters only."),
+    linkedIn: yup.string().max(50, "Maximum of 50 characters only."),
+    twitter: yup.string().max(50, "Maximum of 50 characters only."),
+  }),
+});
+export const profileStudentSchema = yup.object().shape({
+  name: yup.string().matches(name_regex_profile, "Characters only.").required("Required *"),
+  bio: yup.string().max(500, "Maximum of 500 characters only."),
+  socials: yup.object().shape({
+    fb: yup.string().max(50, "Maximum of 50 characters only."),
+    instagram: yup.string().max(50, "Maximum of 50 characters only."),
     twitter: yup.string().max(50, "Maximum of 50 characters only."),
   }),
 });
