43bfac5825d7929d2344c5aa275a93dd12e73b01
R-esentful <raneljohnt@gmail.com>
2023-05-24 13:08:06 +0800

    Updated pages

--- client/src/pages/Dashboard/Attendance.jsx
+++ client/src/pages/Dashboard/Attendance.jsx
@@ -2,11 +2,36 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import axios from "axios";
 function Attendance() {
   const imgRef = useRef(null);
-  const [imageSrc, setImageSrc] = useState("");
+  const [videoLoad, setVideoLoad] = useState(false);
+  const [detected, setDetected] = useState([]);
+  useEffect(() => {
+    if (videoLoad) {
+      setInterval(async () => {
+        const response = await axios.get("/api/v1/detected_faces");
+        setDetected(response.data.faces);
+      }, 3000);
+    }
+  }, [videoLoad]);
 
   return (
     <div>
-      <img src={"/api/v1/video_feed"} alt="Video Feed" />
+      {!videoLoad ? <h1>Loading...</h1> : ""}
+      <img
+        className=""
+        ref={imgRef}
+        src="/api/v1/video_feed"
+        alt="Video Feed"
+        onLoad={() => setVideoLoad(true)}
+      />
+      {detected.length > 0 ? (
+        <ul>
+          {detected.map((face) => (
+            <li key={face}>{face}</li>
+          ))}
+        </ul>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
