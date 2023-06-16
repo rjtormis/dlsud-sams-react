2d13996d222c142b6b478e549a04bab71aba81fc
R-esentful <raneljohnt@gmail.com>
2023-06-16 10:07:42 +0800

    UpdateD

--- client/src/utilities/api.js
+++ client/src/utilities/api.js
@@ -3,4 +3,5 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://dlsud-sams-react-production.up.railway.app",
   // baseURL: "http://127.0.0.1:5000",
+  withCredentials: true,
 });
