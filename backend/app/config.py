1990dfa54bb619554cee950207d3bb9083c88d74
R-esentful <raneljohnt@gmail.com>
2023-06-16 13:54:18 +0800

    Updated config

--- backend/app/config.py
+++ backend/app/config.py
@@ -10,7 +10,6 @@ AWS_KEY = os.getenv("SECRET_KEY")
 
 
 class ApplicationConfig:
-
     SECRET_KEY = os.getenv("SECRET_KEY")
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.getenv("db")
@@ -18,7 +17,7 @@ class ApplicationConfig:
     SQLALCHEMY_POOL_TIMEOUT = 30
 
     JWT_TOKEN_LOCATION = ["cookies"]
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=10)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=5)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(hours=10)
     JWT_SECRET_KEY = os.getenv("SECRET_KEY")
     JWT_ACCESS_COOKIE_NAME = "access_token_cookie"
