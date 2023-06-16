37cddf7fee549102e71f52daa4f488ffb09a38fd
R-esentful <raneljohnt@gmail.com>
2023-06-16 13:36:12 +0800

    Updated

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -19,7 +19,9 @@ app.config.from_object(ApplicationConfig)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
-CORS(app)
+CORS(
+    app, resources={r"*": {"origins": "*", "allow_headers": "*", "expose_headers": "*"}}
+)
 s3 = boto3.client(
     "s3",
     aws_access_key_id=AWS_ACCESS_KEY,
