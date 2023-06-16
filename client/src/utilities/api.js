b4999cd89cd0817678c286e99b48489efe8d54b7
R-esentful <raneljohnt@gmail.com>
2023-06-16 09:51:26 +0800

    Updated

--- client/src/utilities/api.js
+++ client/src/utilities/api.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://dlsud-sams-react-production.up.railway.app",
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: "https://dlsud-sams-react-production.up.railway.app",
+  // baseURL: "http://127.0.0.1:5000",
 });
-
-const access_token = localStorage.getItem("user");
-console.log(access_token);
