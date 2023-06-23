09a361a3913a0c2823ab032bb9cd0a647121248f
R-esentful <raneljohnt@gmail.com>
2023-06-23 21:36:18 +0800

    Added model

---
+++ backend/model_arch/backbones/iresnet.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:384fbfaaa7787d99882140ea0b04d33e55c36f8bc98dc4e4cb30b2b650a911f7
+size 7615
---
+++ backend/model_arch/insightface/face_align.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7040d515dc46b016de73fbb47319a1297b0d7afbed158b92300297aef170239b
+size 3455
